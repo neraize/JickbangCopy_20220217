fcc12c4295bcbc0895c97d01cce5add309b36411
seojungmin <neraize@gmail.com>
2022-02-17 15:17:31 +0900

    어댑터 클래스 getView 기초 작업  - room_list_item의 모양 그대로 나오도록

--- app/src/main/java/com/nepplus/jickbangcopy_20220217/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20220217/adapters/RoomAdapter.kt
@@ -1,7 +1,11 @@
 package com.nepplus.jickbangcopy_20220217.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.nepplus.jickbangcopy_20220217.R
 import com.nepplus.jickbangcopy_20220217.datas.RoomData
 
 class RoomAdapter(
@@ -10,4 +14,16 @@ class RoomAdapter(
     val mList:ArrayList<RoomData>
 ):ArrayAdapter<RoomData>(mContext, resId, mList){
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if(tempRow==null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return  row
+    }
 }
