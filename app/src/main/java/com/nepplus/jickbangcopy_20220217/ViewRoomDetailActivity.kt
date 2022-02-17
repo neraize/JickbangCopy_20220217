a2b2b2ce369a717fc1ae9748acac7de36358d5d8
seojungmin <neraize@gmail.com>
2022-02-17 16:59:11 +0900

    넘겨준 방 데이터(Serializable형태)를 RoomData로 원복 -실제 데이터반영

--- app/src/main/java/com/nepplus/jickbangcopy_20220217/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20220217/ViewRoomDetailActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.jickbangcopy_20220217
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.jickbangcopy_20220217.datas.RoomData
 import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
@@ -9,8 +10,14 @@ class ViewRoomDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
 
-        val price = intent.getStringExtra("priceInfo")
+//        val price = intent.getStringExtra("priceInfo")
+//        txtPrice.text =price
 
-        txtPrice.text =price
+        val roomData =intent.getSerializableExtra("roomInfo") as RoomData
+
+        txtPrice.text = roomData.getFormattedPrice()
+        txtDescription.text = roomData.description
+        txtAddress.text = roomData.address
+        txtFloor.text = roomData.getFormattedFloor()
     }
 }
