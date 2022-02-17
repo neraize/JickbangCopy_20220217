4b1d32791669393572e825270eeae77be1668b34
seojungmin <neraize@gmail.com>
2022-02-17 15:50:52 +0900

    실제 데이터 반영 -설명 문구만 단순반영

--- app/src/main/java/com/nepplus/jickbangcopy_20220217/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20220217/adapters/RoomAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.nepplus.jickbangcopy_20220217.R
 import com.nepplus.jickbangcopy_20220217.datas.RoomData
+import org.w3c.dom.Text
 
 class RoomAdapter(
     val mContext:Context,
@@ -23,6 +25,14 @@ class RoomAdapter(
         }
 
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val txtPrice = row.findViewById<TextView>(R.id.txtPrice)
+        val txtAddressAndFloor = row.findViewById<TextView>(R.id.txtAddressAndFloor)
+        val txtDescription = row.findViewById<TextView>(R.id.txtDescription)
+
+        txtDescription.text = data.description
 
         return  row
     }
